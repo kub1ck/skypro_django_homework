84b522aa3abc8cd5af1c92689f876237daa30e30
Daniil <daniil77089@gmail.com>
2022-11-17 18:46:02 +0300

    hw2

--- ads/models.py
+++ ads/models.py
@@ -1,14 +1,65 @@
 from django.db import models
 
 
-class Ad(models.Model):
-    name = models.CharField(max_length=150)
-    author = models.CharField(max_length=100)
-    price = models.IntegerField()
-    description = models.TextField()
-    address = models.CharField(max_length=200)
-    is_published = models.BooleanField()
+class Location(models.Model):
+    name = models.CharField(max_length=120)
+    lat = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    lng = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+
+    class Meta:
+        verbose_name = 'Адрес'
+        verbose_name_plural = 'Адреса'
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    ROLES = [
+        ('member', 'Пользователь'),
+        ('moderator', 'Модератор'),
+        ('admin', 'Администратор')
+    ]
+
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60, null=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=25)
+    role = models.CharField(max_length=20, choices=ROLES, default='member')
+    age = models.SmallIntegerField()
+    locations = models.ManyToManyField(Location)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Ad(models.Model):
+    name = models.CharField(max_length=150)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    price = models.PositiveIntegerField()
+    description = models.TextField(null=True)
+    is_published = models.BooleanField()
+    image = models.ImageField(upload_to='images/', null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+    def __str__(self):
+        return self.name
--- ads/views.py
+++ ads/views.py
@@ -1,87 +1,59 @@
 import json
 
+from django.core.paginator import Paginator
+from django.db.models import Count, Q
 from django.http import JsonResponse, Http404
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
-from ads.models import Ad, Category
+from ads.models import Ad, Category, User, Location
+from ads_dj import settings
 
 
 def index(request):
     return JsonResponse({"status": "ok"}, status=200)
 
 
+"""
+    AD
+"""
+
+
 @method_decorator(csrf_exempt, name="dispatch")
-class AdView(View):
-    def get(self, request):
-        data = Ad.objects.all()
+class AdListView(ListView):
+    model = Ad
 
-        response = []
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        self.object_list = self.object_list.select_related("author", "category").order_by("-price")
 
-        for ad in data:
-            response.append({
+        paginator = Paginator(self.object_list, settings.TOTAL_ON_PAGE)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+
+        ads = []
+
+        for ad in page_obj:
+            ads.append({
                 "id": ad.id,
                 "name": ad.name,
-                "author": ad.author,
+                "author": ad.author_id,
                 "price": ad.price,
                 "description": ad.description,
-                "address": ad.address,
-                "is_published": ad.is_published
+                "is_published": ad.is_published,
+                "category": ad.category_id,
+                "image": ad.image.url if ad.image else None
             })
 
-        return JsonResponse(response, safe=False)
-
-    def post(self, request):
-        data = json.loads(request.body)
-
-        ad = Ad.objects.create(
-            name=data['name'],
-            author=data['author'],
-            description=data['description'],
-            price=data['price'],
-            address=data['address'],
-            is_published=data['is_published']
-        )
-
-        return JsonResponse({
-            "id": ad.id,
-            "name": ad.name,
-            "author": ad.author,
-            "price": ad.price,
-            "description": ad.description,
-            "address": ad.address,
-            "is_published": ad.is_published
-        })
-
-
-@method_decorator(csrf_exempt, name="dispatch")
-class CategoryView(View):
-    def get(self, request):
-        data = Category.objects.all()
-
-        response = []
-
-        for category in data:
-            response.append({
-                "id": category.id,
-                "name": category.name
-            })
+        response = {
+            "items": ads,
+            "total": paginator.count,
+            "num_pages": paginator.num_pages
+        }
 
         return JsonResponse(response, safe=False)
-
-    def post(self, request):
-        data = json.loads(request.body)
-
-        category = Category.objects.create(
-            name=data['name'],
-        )
-
-        return JsonResponse({
-            "id": category.id,
-            "name": category.name,
-        })
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -92,30 +64,371 @@ class AdDetailView(DetailView):
         try:
             ad = self.get_object()
         except Http404:
-            return JsonResponse({'Error': 'Not found'}, status=404)
+            return JsonResponse({"Error": "Not found"}, status=404)
 
         return JsonResponse({
             "id": ad.id,
             "name": ad.name,
-            "author": ad.author,
+            "author": ad.author_id,
             "price": ad.price,
             "description": ad.description,
-            "address": ad.address,
-            "is_published": ad.is_published
+            "is_published": ad.is_published,
+            "category": ad.category_id,
+            "image": ad.image.url if ad.image else None
         })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class AdCreateView(CreateView):
+    model = Ad
+    fields = ["name", "author", "price", "description", "is_published", "category"]
+
+    def post(self, request, *args, **kwargs):
+        ad_data = json.loads(request.body)
+
+        ad = Ad.objects.create(
+            name=ad_data["name"],
+            author_id=ad_data["author"],
+            price=ad_data["price"],
+            description=ad_data["description"],
+            is_published=ad_data["is_published"],
+            category_id=ad_data["category"]
+        )
+
+        return JsonResponse({
+            "id": ad.id,
+            "name": ad.name,
+            "author": ad.author_id,
+            "price": ad.price,
+            "description": ad.description,
+            "is_published": ad.is_published,
+            "category": ad.category_id
+        })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class AdUpdateView(UpdateView):
+    model = Ad
+    fields = ["name", "author", "price", "description", "is_published", "category"]
+
+    def patch(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        ad_data = json.loads(request.body)
+
+        self.object.name = ad_data["name"]
+        self.object.author_id = ad_data["author"]
+        self.object.price = ad_data["price"]
+        self.object.description = ad_data["description"]
+        self.object.is_published = ad_data["is_published"]
+        self.object.category_id = ad_data["category"]
+
+        self.object.save()
+
+        return JsonResponse({
+            "id": self.object.id,
+            "name": self.object.name,
+            "author": self.object.author_id,
+            "price": self.object.price,
+            "description": self.object.description,
+            "is_published": self.object.is_published,
+            "category": self.object.category_id
+        })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class AdImageView(UpdateView):
+    model = Ad
+    fields = ["name", "image"]
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        self.object.image = request.FILES["image"]
+
+        self.object.save()
+
+        return JsonResponse({
+            'id': self.object.id,
+            'name': self.object.name,
+            'image': self.object.image.url if self.object.image else None
+        })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class AdDeleteView(DeleteView):
+    model = Ad
+    success_url = '/'
+
+    def delete(self, request, *args, **kwargs):
+        super().delete(request, *args, **kwargs)
+
+        return JsonResponse({"status": "ok"}, status=200)
+
+
+"""
+    CATEGORY
+"""
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class CategoryListView(ListView):
+    model = Category
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+
+        paginator = Paginator(self.object_list, settings.TOTAL_ON_PAGE)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+
+        categories = []
+
+        for category in page_obj:
+            categories.append({
+                "id": category.id,
+                "name": category.name
+            })
+
+        response = {
+            "items": categories,
+            "total": paginator.count,
+            "num_pages": paginator.num_pages
+        }
+
+        return JsonResponse(response, safe=False)
 
 
 @method_decorator(csrf_exempt, name="dispatch")
 class CategoryDetailView(DetailView):
     model = Category
 
-    def get(self, request, *args, **kwars):
+    def get(self, request, *args, **kwargs):
         try:
             category = self.get_object()
         except Http404:
-            return JsonResponse({'Error': 'Not found'}, status=404)
+            return JsonResponse({"Error": "Not found"}, status=404)
 
         return JsonResponse({
             "id": category.id,
             "name": category.name
         })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ["name"]
+
+    def post(self, request, *args, **kwargs):
+        category_data = json.loads(request.body)
+
+        category = Category.objects.create(
+            name=category_data["name"]
+        )
+
+        return JsonResponse({
+            "name": category.name
+        })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ["name"]
+
+    def patch(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        category_data = json.loads(request.body)
+
+        self.object.name = category_data["name"]
+
+        self.object.save()
+
+        return JsonResponse({
+            "name": self.object.name
+        })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = '/'
+
+    def delete(self, request, *args, **kwargs):
+        super().delete(request, *args, **kwargs)
+
+        return JsonResponse({"status": "ok"}, status=200)
+
+
+"""
+    USER
+"""
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class UserListView(ListView):
+    model = User
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+
+        paginator = Paginator(self.object_list, settings.TOTAL_ON_PAGE)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+
+        users = []
+
+        for user in page_obj:
+            users.append({
+                "id": user.id,
+                "username": user.username,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "role": user.role,
+                "age": user.age,
+                "location": list(map(str, user.locations.all())),
+            })
+
+        response = {
+            "items": users,
+            "total": paginator.count,
+            "num_pages": paginator.num_pages
+        }
+
+        return JsonResponse(response, safe=False)
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class UserDetailView(DetailView):
+    model = User
+
+    def get(self, request, *args, **kwargs):
+        try:
+            user = self.get_object()
+        except Http404:
+            return JsonResponse({"Error": "Not found"}, status=404)
+
+        return JsonResponse({
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "password": user.password,
+            "role": user.role,
+            "age": user.age,
+            "locations": list(map(str, user.locations.all()))
+        })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class UserCreateView(CreateView):
+    model = User
+    fields = ["first_name", "last_name", "username", "password", "role", "age", "locations"]
+
+    def post(self, request, *args, **kwargs):
+        user_data = json.loads(request.body)
+
+        user = User.objects.create(
+            first_name=user_data["first_name"],
+            last_name=user_data["last_name"],
+            username=user_data["username"],
+            password=user_data["password"],
+            role=user_data["role"],
+            age=user_data["age"]
+        )
+
+        for location in user_data["locations"]:
+            location_object, created = Location.objects.get_or_create(name=location)
+            user.locations.add(location_object)
+
+        user.save()
+
+        return JsonResponse({
+            "id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "password": user.password,
+            "role": user.role,
+            "age": user.age,
+            "locations": list(map(str, user.locations.all()))
+        })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ["first_name", "last_name", "username", "password", "role", "age", "locations"]
+
+    def patch(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        user_data = json.loads(request.body)
+
+        self.object.first_name = user_data["first_name"]
+        self.object.last_name = user_data["last_name"]
+        self.object.username = user_data["username"]
+        self.object.password = user_data["password"]
+        self.object.role = user_data["role"]
+        self.object.age = user_data["age"]
+
+        for location in user_data["locations"]:
+            location_object, created = Location.objects.get_or_create(name=location)
+            self.object.locations.add(location_object)
+
+        self.object.save()
+
+        return JsonResponse({
+            "id": self.object.id,
+            "first_name": self.object.first_name,
+            "last_name": self.object.last_name,
+            "username": self.object.username,
+            "password": self.object.password,
+            "role": self.object.role,
+            "age": self.object.age,
+            "locations": list(map(str, self.object.locations.all()))
+        })
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class UserDeleteView(DeleteView):
+    model = User
+    success_url = '/'
+
+    def delete(self, request, *args, **kwargs):
+        super().delete(request, *args, **kwargs)
+
+        return JsonResponse({"status": "ok"}, status=200)
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class UserAdDetailView(View):
+    def get(self, request):
+        user_qs = User.objects\
+            .prefetch_related("locations")\
+            .annotate(total_ads=Count('ad', filter=Q(ad__is_published=True)))
+
+        paginator = Paginator(user_qs, settings.TOTAL_ON_PAGE)
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+
+        users = []
+
+        for user in page_obj:
+            users.append({
+                "id": user.id,
+                "username": user.username,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "role": user.role,
+                "age": user.age,
+                "location": list(map(str, user.locations.all())),
+                "total_ads": user.total_ads
+            })
+
+        response = {
+            "items": users,
+            "total": paginator.count,
+            "num_pages": paginator.num_pages
+        }
+
+        return JsonResponse(response, safe=False)
